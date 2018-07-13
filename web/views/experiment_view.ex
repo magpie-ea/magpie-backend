e0f25d18f1fceafda0e4bf735fc981b80627a9ac
Xiang Ji <hi@xiangji.me>
2018-07-13 15:23:54 +0200

    Fix indentation in experiment_view.ex

--- web/views/experiment_view.ex
+++ web/views/experiment_view.ex
@@ -3,41 +3,41 @@ defmodule BABE.ExperimentView do
 
   # From https://medium.com/@chipdean/phoenix-array-input-field-implementation-7ec0fe0949d
   def array_input(form, field) do
-      values = Phoenix.HTML.Form.input_value(form, field) || [""]
-      id = Phoenix.HTML.Form.input_id(form, field)
-      type = Phoenix.HTML.Form.input_type(form, field)
-      content_tag :ol, id: container_id(id), class: "input_container", data: [index: Enum.count(values)] do
-      values
-        |> Enum.with_index()
-        |> Enum.map(fn {value, index} ->
-          new_id = id <> "_#{index}"
-          input_opts = [
-            name: new_field_name(form, field),
-            value: value,
-            id: new_id,
-            class: "form-control"
-          ]
-          form_elements(form, field, value, index)
-        end)
-      end
-    end
-
-    defp form_elements(form, field, value, index) do
-      type = Phoenix.HTML.Form.input_type(form, field)
-      id = Phoenix.HTML.Form.input_id(form, field)
-      new_id = id <> "_#{index}"
-      input_opts = [
-        name: new_field_name(form, field),
-        value: value,
-        id: new_id,
-        class: "form-control"
-      ]
-      content_tag :li do
-        [
-          apply(Phoenix.HTML.Form, type, [form, field, input_opts]),
-          link("Remove", to: "#", data: [id: new_id], title: "Remove", class: "remove-form-field")
+    values = Phoenix.HTML.Form.input_value(form, field) || [""]
+    id = Phoenix.HTML.Form.input_id(form, field)
+    type = Phoenix.HTML.Form.input_type(form, field)
+    content_tag :ol, id: container_id(id), class: "input_container", data: [index: Enum.count(values)] do
+    values
+      |> Enum.with_index()
+      |> Enum.map(fn {value, index} ->
+        new_id = id <> "_#{index}"
+        input_opts = [
+          name: new_field_name(form, field),
+          value: value,
+          id: new_id,
+          class: "form-control"
         ]
-      end
+        form_elements(form, field, value, index)
+      end)
+    end
+  end
+
+  defp form_elements(form, field, value, index) do
+    type = Phoenix.HTML.Form.input_type(form, field)
+    id = Phoenix.HTML.Form.input_id(form, field)
+    new_id = id <> "_#{index}"
+    input_opts = [
+      name: new_field_name(form, field),
+      value: value,
+      id: new_id,
+      class: "form-control"
+    ]
+    content_tag :li do
+      [
+        apply(Phoenix.HTML.Form, type, [form, field, input_opts]),
+        link("Remove", to: "#", data: [id: new_id], title: "Remove", class: "remove-form-field")
+      ]
+    end
   end
 
   defp container_id(id), do: id <> "_container"

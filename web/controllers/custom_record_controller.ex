6459378b04d80f1976156a69b25e1e199e61a65d
Xiang Ji <hi@xiangji.me>
2018-08-10 23:56:42 +0200

    Enable editing of existing custom records. #23

--- web/controllers/custom_record_controller.ex
+++ web/controllers/custom_record_controller.ex
@@ -18,29 +18,32 @@ defmodule BABE.CustomRecordController do
     render(conn, "new.html", changeset: changeset)
   end
 
+  defp convert_uploaded_data(upload) do
+    case upload.content_type do
+      "application/json" ->
+        upload.path
+        |> File.read!()
+        |> Poison.decode!()
+
+      "text/csv" ->
+        upload.path
+        |> File.stream!()
+        |> CSV.decode!(headers: true)
+        # We shouldn't need to manually verify that the rows are valid. The decode! should do it for us
+        # |> Stream.filter(fn({k, v}) -> k == :ok end)
+        # |> Stream.map(fn({k, v}) -> v end)
+        |> Enum.take_every(1)
+
+      _ ->
+        nil
+    end
+  end
+
   def create(conn, %{"custom_record" => custom_record_params}) do
     upload = custom_record_params["record"]
 
     try do
-      content =
-        case upload.content_type do
-          "application/json" ->
-            upload.path
-            |> File.read!()
-            |> Poison.decode!()
-
-          "text/csv" ->
-            upload.path
-            |> File.stream!()
-            |> CSV.decode!(headers: true)
-            # We shouldn't need to manually verify that the rows are valid. The decode! should do it for us
-            # |> Stream.filter(fn({k, v}) -> k == :ok end)
-            # |> Stream.map(fn({k, v}) -> v end)
-            |> Enum.take_every(1)
-
-          _ ->
-            nil
-        end
+      content = convert_uploaded_data(upload)
 
       if content == nil do
         conn
@@ -89,7 +92,59 @@ defmodule BABE.CustomRecordController do
   def edit(conn, %{"id" => id}) do
     custom_record = Repo.get!(CustomRecord, id)
     changeset = CustomRecord.changeset(custom_record)
-    render(conn, "edit.html", record: custom_record, changeset: changeset)
+    render(conn, "edit.html", custom_record: custom_record, changeset: changeset)
+  end
+
+  def update(conn, %{"id" => id, "custom_record" => custom_record_params}) do
+    custom_record = Repo.get!(CustomRecord, id)
+
+    upload = custom_record_params["record"]
+
+    try do
+      content = convert_uploaded_data(upload)
+
+      if content == nil do
+        conn
+        |> put_flash(:error, "Make sure the file extension is either .csv or .json")
+        |> render("edit.html",
+          changeset: CustomRecord.changeset(%CustomRecord{name: custom_record_params["name"]})
+        )
+        |> halt
+      end
+
+      changeset =
+        CustomRecord.changeset(custom_record, %{
+          name: custom_record_params["name"],
+          record: content
+        })
+
+      case Repo.update(changeset) do
+        {:ok, custom_record} ->
+          conn
+          |> put_flash(:info, "#{custom_record.name} updated successfully.")
+          |> redirect(to: custom_record_path(conn, :index))
+
+        {:error, changeset} ->
+          render(conn, "edit.html", custom_record: custom_record, changeset: changeset)
+      end
+    rescue
+      UndefinedFunctionError ->
+        conn
+        |> put_flash(:error, "No file selected.")
+        |> render("new.html",
+          changeset: CustomRecord.changeset(%CustomRecord{name: custom_record_params["name"]})
+        )
+
+      _ ->
+        conn
+        |> put_flash(
+          :error,
+          "Some rows/contents in the file weren't able to be parsed correctly. Please check the formatting."
+        )
+        |> render("new.html",
+          changeset: CustomRecord.changeset(%CustomRecord{name: custom_record_params["name"]})
+        )
+    end
   end
 
   def delete(conn, %{"id" => id}) do

29be5315954726d01b16a4b729e621be88b86288
Xiang Ji <hi@xiangji.me>
2018-08-10 21:57:22 +0200

    UI Changes

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -1,7 +1,7 @@
 defmodule BABE.PageController do
   @moduledoc false
-
   use BABE.Web, :controller
+  plug(BasicAuth, use_config: {:babe, :authentication})
 
   def index(conn, _params) do
     render(conn, "index.html")

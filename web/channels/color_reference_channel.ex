ac986b9a14c5c5d051ef06ab9e6dbeface563dbd
Xiang Ji <hi@xiangji.me>
2018-12-01 22:31:36 +0100

    Document functions in ColorReferenceChannel

--- web/channels/color_reference_channel.ex
+++ web/channels/color_reference_channel.ex
@@ -1,11 +1,12 @@
 defmodule BABE.ColorReferenceChannel do
   use BABE.Web, :channel
   alias BABE.Presence
-  import Ecto
 
   @num_users_per_game 2
 
-
+  @doc """
+  The first step after establishing connection for any participant is to log in with a (in most cases randomly generated in the frontend) user_id
+  """
   def join("color_reference:user:" <> user_id, _payload, socket) do
     if socket.assigns.user_id == user_id do
       {:ok, socket}
@@ -14,6 +15,9 @@ defmodule BABE.ColorReferenceChannel do
     end
   end
 
+  @doc """
+  After a participant successfully logs in, they should join the lobby.
+  """
   def join("color_reference:lobby", _payload, socket) do
     Presence.track(socket, socket.assigns.user_id, %{
       online_at: inspect(System.system_time(:second))
@@ -23,42 +27,13 @@ defmodule BABE.ColorReferenceChannel do
     {:ok, socket}
   end
 
-  def join("color_reference:lounge:" <> lounge_id, _payload, socket) do
-    existing_users = Map.keys(Presence.list(socket))
-    IO.puts("lounge: #{lounge_id}")
-    IO.inspect(Presence.list(socket))
-
-    if length(existing_users) >= @num_users_per_game do
-      {:error, %{reason: "Lounge already full! Please refresh."}}
-    else
-      Presence.track(socket, socket.assigns.user_id, %{
-        online_at: inspect(System.system_time(:second))
-      })
-      {:ok, socket}
-    end
-
-  end
-
-  # def join("color_reference:" <> lounge, payload, socket) do
-  #   if authorized?(payload) do
-  #     {:ok, socket}
-  #   else
-  #     {:error, %{reason: "unauthorized"}}
-  #   end
-  # end
-
   @doc """
-  What to do:
-  - Check the number of users present in the lobby when a new user joins
-  - If the target number (in most cases 2) is reached, send out game start message, with a randomly generated lobby ID
-  - Probably should also just disconnect all the users in the lobby from the server side afterwards to avoid problems? Or maybe not. I might even define the function such that when the number of users connected somehow exceeds 2 (e.g. when the 2nd and the 3rd user join simultaneously), always select the first two presences and send messages to them. Might work let's see.
-    Can also write a test about such cases.
+  After a user joins the lobby, check the target condition (in this experiment the condition is num_waiting_participants==2). If the condition is met, send out a game start message to all participants, with a randomly generated lobby ID
   """
   def handle_info(:after_user_join_lobby, socket) do
     # The `list` function returns presences for a topic.
-
+    # The keys are user_ids
     existing_users = Map.keys(Presence.list(socket))
-    IO.inspect(existing_users)
 
     if length(existing_users) >= @num_users_per_game do
       [user1_id | [user2_id | _]] = existing_users
@@ -74,24 +49,42 @@ defmodule BABE.ColorReferenceChannel do
         role: "listener"
       })
 
-      # If the users leave the lobby channel already, don't think we need to manually remove them here.
+      # If the users leave the lobby channel on their own, don't think we need to manually remove them here.
       # Presence.untrack(socket, user1_id)
       # Presence.untrack(socket, user2_id)
-      IO.inspect(Presence.list(socket))
+      # IO.inspect(Presence.list(socket))
     end
 
-    # If the number of users in the lobby has reached the target number, start the game
-    # Else track this current user as well
-    # if length(Presence.list(socket)["in_lobby"][:metas]) > num_users_per_game - 1 do
-    #   broadcast!(socket, "game_start", %{lounge: "asdf"})
-    #   # send(dest, message)
-    # else
-    #   Presence.track(socket, "in_lobby", %{
-    #     online_at: inspect(System.system_time(:second))
-    #   })
-    # end
+    {:noreply, socket}
+  end
 
-    # :noreply seems to mean that we don't send any message/event to the client anyways.
+  @doc """
+  If the conditions are met (e.g. there are enough participants waiting in the lobby), the server tells selected participants to join a lounge to start the experiment.
+
+  This function handles what happens when the participants send in the join request.
+  """
+  def join("color_reference:lounge:" <> lounge_id, _payload, socket) do
+    existing_users = Map.keys(Presence.list(socket))
+    IO.puts("lounge: #{lounge_id}")
+    IO.inspect(Presence.list(socket))
+
+    # Maybe there will be errors where somehow more users than intended try to join. We can only reject the last user if that's the case.
+    if length(existing_users) >= @num_users_per_game do
+      {:error, %{reason: "Game already full! Please refresh."}}
+    else
+      Presence.track(socket, socket.assigns.user_id, %{
+        online_at: inspect(System.system_time(:second))
+      })
+
+      {:ok, socket}
+    end
+  end
+
+  @doc """
+  This handles new messages from the clients and broadcast them to everybody susbcribed to the same topic (i.e. who joined the same lounge).
+  """
+  def handle_in("new_msg", payload, socket) do
+    broadcast(socket, "new_msg", payload)
     {:noreply, socket}
   end
 
@@ -101,18 +94,4 @@ defmodule BABE.ColorReferenceChannel do
   # def handle_in("ping", payload, socket) do
   #   {:reply, {:ok, payload}, socket}
   # end
-
-  # It is also common to receive messages from the client and
-  # broadcast to *everyone* in the current topic (color_reference:lounge_id).
-  def handle_in("new_msg", payload, socket) do
-    broadcast(socket, "new_msg", payload)
-    {:noreply, socket}
-  end
-
-  # Add authorization logic here as required.
-  # By default it just returns true.
-  # We might be able to add some sort of authorization token, or we may just spare this step for now.
-  defp authorized?(_payload) do
-    true
-  end
 end

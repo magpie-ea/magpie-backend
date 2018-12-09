6c50b6cef5083212f06aa9bd27c38df5cfcf3b11
Xiang Ji <hi@xiangji.me>
2018-12-09 14:04:48 +0100

    Add distillery as dependency

--- mix.exs
+++ mix.exs
@@ -2,15 +2,17 @@ defmodule BABE.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :babe,
-     version: "0.0.1",
-     elixir: "~> 1.2",
-     elixirc_paths: elixirc_paths(Mix.env),
-     compilers: [:phoenix, :gettext] ++ Mix.compilers,
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     aliases: aliases(),
-     deps: deps()]
+    [
+      app: :babe,
+      version: "0.0.1",
+      elixir: "~> 1.2",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application.
@@ -18,36 +20,49 @@ defmodule BABE.Mixfile do
 
   # Type `mix help compile.app` for more information.
   def application do
-    [mod: {BABE, []},
-     applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex, :calendar]]
+    [
+      mod: {BABE, []},
+      applications: [
+        :phoenix,
+        :phoenix_pubsub,
+        :phoenix_html,
+        :cowboy,
+        :logger,
+        :gettext,
+        :phoenix_ecto,
+        :postgrex,
+        :calendar
+      ]
+    ]
   end
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "web", "test/support"]
-  defp elixirc_paths(_),     do: ["lib", "web"]
+  defp elixirc_paths(_), do: ["lib", "web"]
 
   # Specifies your project dependencies.
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.3.0"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.0"},
-     {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.6"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"},
-     {:poison, "~> 3.1"},
-     {:iteraptor, "~> 0.7.0"},
-     {:cors_plug, "~> 1.2"},
-     {:csv, "~> 2.1"},
-     {:basic_auth, "~> 2.2.2"},
-     {:calendar, "~> 0.17.2"},
-     # Error checking and linting
-     {:credo, "~> 0.5", only: [:dev, :test]},
-     {:dogma, "~> 0.1", only: [:dev]}
+    [
+      {:phoenix, "~> 1.3.0"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix_ecto, "~> 3.0"},
+      {:postgrex, ">= 0.0.0"},
+      {:phoenix_html, "~> 2.6"},
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:gettext, "~> 0.11"},
+      {:cowboy, "~> 1.0"},
+      {:poison, "~> 3.1"},
+      {:iteraptor, "~> 0.7.0"},
+      {:cors_plug, "~> 1.2"},
+      {:csv, "~> 2.1"},
+      {:basic_auth, "~> 2.2.2"},
+      {:calendar, "~> 0.17.2"},
+      {:distillery, "~> 2.0"},
+      # Error checking and linting
+      {:credo, "~> 0.5", only: [:dev, :test]},
+      {:dogma, "~> 0.1", only: [:dev]}
     ]
   end
 
@@ -58,8 +73,10 @@ defmodule BABE.Mixfile do
   #
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
-    ["ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
-     "ecto.reset": ["ecto.drop", "ecto.setup"],
-     test: ["ecto.create --quiet", "ecto.migrate", "test"]]
+    [
+      "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
+      "ecto.reset": ["ecto.drop", "ecto.setup"],
+      test: ["ecto.create --quiet", "ecto.migrate", "test"]
+    ]
   end
 end

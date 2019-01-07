f97e0555f49726e0756fd4478aacd0de7abfeca5
Xiang Ji <hi@xiangji.me>
2019-01-07 16:45:13 +0100

    No need to enable sockets on the server side

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -18,7 +18,7 @@ import 'phoenix_html';
 // Local files can be imported directly using relative
 // paths "./socket" or full ones "web/static/js/socket".
 
-import socket from './socket';
+// import socket from './socket';
 
 // From https://medium.com/@chipdean/phoenix-array-input-field-implementation-7ec0fe0949d
 window.onload = () => {

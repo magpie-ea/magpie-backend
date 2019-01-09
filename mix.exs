d22ffa150fe78b5f76a63754422c481772bdfc22
Xiang Ji <hi@xiangji.me>
2019-01-09 21:32:32 +0100

    Update version number

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule BABE.Mixfile do
   def project do
     [
       app: :babe,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),

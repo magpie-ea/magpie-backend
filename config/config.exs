915b8b9e315310882d339791b299fa4afd2c9ea8
Xiang Ji <hi@xiangji.me>
2019-01-11 22:42:35 +0100

    Need to wrap the if expression in parens

--- config/config.exs
+++ config/config.exs
@@ -24,11 +24,14 @@ config :logger, :console,
 # The local timezone where the app is deployed.
 config :babe, :timezone, "Europe/Berlin"
 
-config :babe, :no_basic_auth, if(Map.has_key?(System.get_env(), "BABE_NO_BASIC_AUTH")) do
-  true
-else
-  false
-end
+no_basic_auth =
+  config :babe,
+         :no_basic_auth,
+         (if Map.has_key?(System.get_env(), "BABE_NO_BASIC_AUTH") do
+            true
+          else
+            false
+          end)
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.

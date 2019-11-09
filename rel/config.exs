d320dc2d9a30e8b2d5bf95d6433ef0bde73e87a1
Xiang Ji <hi@xiangji.me>
2019-11-09 23:11:31 +0100

    Regenerate rel/config.exs for Distillery 2.1

--- rel/config.exs
+++ rel/config.exs
@@ -7,21 +7,21 @@
 |> Path.wildcard()
 |> Enum.map(&Code.eval_file(&1))
 
-use Mix.Releases.Config,
-  # This sets the default release built by `mix release`
-  default_release: :default,
-  # This sets the default environment used by `mix release`
-  default_environment: Mix.env()
+use Distillery.Releases.Config,
+    # This sets the default release built by `mix distillery.release`
+    default_release: :default,
+    # This sets the default environment used by `mix distillery.release`
+    default_environment: Mix.env()
 
 # For a full list of config options for both releases
 # and environments, visit https://hexdocs.pm/distillery/config/distillery.html
+
 
 # You may define one or more environments in this file,
 # an environment's settings will override those of a release
 # when building in that environment, this combination of release
 # and environment configuration is called a profile
 
-# Normally no need to build a release for :dev environment.
 environment :dev do
   # If you are running Phoenix, you should make sure that
   # server: true is set and the code reloader is disabled,
@@ -29,30 +29,27 @@ environment :dev do
   # It is recommended that you build with MIX_ENV=prod and pass
   # the --env flag to Distillery explicitly if you want to use
   # dev mode.
-  set(dev_mode: true)
-  set(include_erts: false)
-  set(cookie: :"Iz^@CU&TG~_mpi![Ra~sxP[69Hc{4MA*;`kt/1Hr5@B&S8T::TY8e~[Y$z9O.~)*")
+  set dev_mode: true
+  set include_erts: false
+  set cookie: :"bGD1}(Pxpq.Dr/{@8@A7MnH/vhK[Pv:<Rk8ST@9*@>^%D}x^Mp6?${m4U&f;[ID."
 end
 
 environment :prod do
-  set(include_erts: true)
-  set(include_src: false)
-  # Set a default cookie anyways.
-  set(cookie: :"<_7Aws;~}M^$|k=fO>f)%lG!.>22V,2z9inM7WuXWc}MB1(3!nvJFndij.t}`$uL")
-  set(vm_args: "rel/vm.args")
+  set include_erts: true
+  set include_src: false
+  set cookie: :"Zm%&[jE:nHWym0dQ~w;8f?>}N7!;=gK5W3Ev67iEy?i%{CBU|:hY58M[q9s[co)8"
+  set vm_args: "rel/vm.args"
 end
 
 # You may define one or more releases in this file.
 # If you have not set a default release, or selected one
-# when running `mix release`, the first release in the file
+# when running `mix distillery.release`, the first release in the file
 # will be used by default
 
 release :magpie do
-  set(version: current_version(:magpie))
-
-  set(
-    applications: [
-      :runtime_tools
-    ]
-  )
+  set version: current_version(:magpie)
+  set applications: [
+    :runtime_tools
+  ]
 end
+

a67a3796b701189df65baa22a9e50a92452fdb8c
Xiang Ji <hi@xiangji.me>
2021-08-17 20:21:09 +0200

    (#123) Fix migration

--- priv/repo/migrations/20210728180507_add_num_players_to_experiments.exs
+++ priv/repo/migrations/20210728180507_add_num_players_to_experiments.exs
@@ -2,6 +2,17 @@ defmodule Magpie.Repo.Migrations.AddNumPlayersToExperiments do
   use Ecto.Migration
 
   def up do
+    drop(
+      index(:experiment_statuses, [:experiment_id, :generation, :variant, :chain])
+    )
+
+    drop(
+      index(
+        :experiment_results,
+        [:experiment_id, :generation, :variant, :chain, :is_intermediate]
+      )
+    )
+
     alter table(:experiments) do
       add :num_players, :integer, default: 1, null: false
       add :is_interactive, :boolean, default: false, null: false
@@ -36,6 +47,23 @@ defmodule Magpie.Repo.Migrations.AddNumPlayersToExperiments do
   end
 
   def down do
+    drop(
+      index(
+        :experiment_results,
+        [:experiment_id, :generation, :variant, :chain, :player, :is_intermediate]
+      )
+    )
+
+    drop(
+      index(:experiment_statuses, [:experiment_id, :generation, :variant, :chain, :player])
+    )
+
+    # Maybe we'll run a query bypassing all the other fields.
+    drop(index(:experiment_results, [:experiment_id, :player]))
+
+    drop(index(:experiment_statuses, [:experiment_id, :player]))
+
+
     alter table(:experiments) do
       remove :num_players, :integer, null: true
       remove :is_interactive, :boolean, default: false, null: false
@@ -48,5 +76,17 @@ defmodule Magpie.Repo.Migrations.AddNumPlayersToExperiments do
     alter table(:experiment_statuses) do
       remove :player, :integer, null: false
     end
+
+    create(
+      index(
+        :experiment_results,
+        [:experiment_id, :generation, :variant, :chain, :is_intermediate],
+        unique: false
+      )
+    )
+
+    create(
+      index(:experiment_statuses, [:experiment_id, :generation, :variant, :chain], unique: true)
+    )
   end
 end

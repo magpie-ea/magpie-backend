b8d5740e543d73fd66435de83cdf69881046b608
Xiang Ji <hi@xiangji.me>
2022-08-05 21:17:19 +0200

    Refactoring ParticipantChannel

--- lib/magpie_web/channels/participant_channel.ex
+++ lib/magpie_web/channels/participant_channel.ex
@@ -4,28 +4,34 @@ defmodule Magpie.ParticipantChannel do
   """
 
   use MagpieWeb, :channel
+
+  alias Ecto.Multi
   alias Magpie.Experiments
   alias Magpie.Experiments.{AssignmentIdentifier, ExperimentStatus, ExperimentResult}
+  alias Magpie.Presence
   alias Magpie.Repo
-  alias Ecto.Multi
+
+  alias Magpie.Experiments.WaitingQueueWorker
+
   require Ecto.Query
   require Logger
 
   @doc """
-  The first step after establishing connection for any participant is to log in with a (in most cases randomly generated in the frontend) participant_id
+  The first step after establishing connection for any participant is to log in with a (randomly generated in the frontend) participant_id
   """
   def join("participant:" <> participant_id, _payload, socket) do
     # The participant_id should have been stored in the socket assigns already, and should match what the client tries to send us.
     if socket.assigns.participant_id == participant_id do
-      send(self(), :after_participant_join)
-
       :ok =
         Magpie.Experiments.ChannelWatcher.monitor(
           :participants,
           self(),
-          {__MODULE__, :handle_leave, [socket]}
+          {__MODULE__, :handle_leave, [socket, participant_id]}
         )
 
+      send(self(), :after_participant_join)
+
+      # Seems that we'll have to first return {:ok, socket} before we're able to broadcast anything to them? I'm not actually sure anymore lol. Let's still do it this way anyways.
       {:ok, socket}
     else
       {:error, %{reason: "unauthorized"}}
@@ -35,9 +41,12 @@ defmodule Magpie.ParticipantChannel do
   @doc """
   Reset the experiment status when the user leaves halfway through (e.g. closes the tab)
 
-  N.B.: This callback might not catch situations where the connection times out, etc. A GenServer as mentioned in https://stackoverflow.com/questions/33934029/how-to-detect-if-a-user-left-a-phoenix-channel-due-to-a-network-disconnect could be useful.
+  An additional ChannelWatcher is also implemented according to https://stackoverflow.com/questions/33934029/how-to-detect-if-a-user-left-a-phoenix-channel-due-to-a-network-disconnect so that handle_leave/1 is invoked also when the user loses connection, not just when they close the tab.
   """
-  def handle_leave(socket) do
+  def handle_leave(socket, participant_id) do
+    # If the user is still in the waiting queue, remove them from it.
+    WaitingQueueWorker.dequeue_participant(participant_id)
+
     # We should probably reset the whole set of statuses, even if only one participant breaks out.
     # Note that we only deal with experiments with status 1, i.e. if any of other participants already saved the entire result and thus set the status to 2, the results would not be affected.
     Experiments.reset_in_progress_assignments_for_interactive_exp(
@@ -46,21 +55,18 @@ defmodule Magpie.ParticipantChannel do
   end
 
   def handle_info(:after_participant_join, socket) do
-    # Broadcast the tuple <variant-nr, chain-nr, generation-nr, player-nr> to the user.
-    # We know that the experiment is available already when we accept the participant in ParticipantSocket.
-    # However we can only broadcast the message after they've joined to the channel.
-    broadcast(
-      socket,
-      "experiment_available",
-      socket.assigns.assignment_identifier
-    )
+    case Slots.get_and_set_to_in_progress_next_free_slot(socket.assigns.experiment_id) do
+      {:ok, slot_identifier} ->
+        broadcast(socket, "slot_available", slot_identifier)
 
-    # broadcast(socket, "experiment_available", %{
-    #   variant: socket.assigns.assignment_status.variant,
-    #   chain: socket.assigns.assignment_status.chain,
-    #   generation: socket.assigns.assignment_status.generation,
-    #   player: socket.assigns.assignment_status.player
-    # })
+      :no_free_slot_available ->
+        :ok = WaitingQueueWorker.queue_participant(socket.assigns.participant_id)
+
+        broadcast(socket, "waiting_in_queue", %{})
+
+      error ->
+        broadcast(socket, "error_upon_joining", %{error: inspect(error)})
+    end
 
     {:noreply, socket}
   end
@@ -93,11 +99,16 @@ defmodule Magpie.ParticipantChannel do
         Magpie.Experiments.ChannelWatcher.demonitor(:participants, self())
 
         # Tell all clients that are waiting for results of this experiment that the experiment is finished, and send them the results.
-        Magpie.Endpoint.broadcast!(
-          "iterated_lobby:#{AssignmentIdentifier.to_string(socket.assigns.assignment_identifier)}",
-          "finished",
-          %{results: payload["results"]}
-        )
+        # I think now we can actually simply ask the next-in-line to join again.
+        # So, we should actually broadcast in private to the ParticipantChannel of that user directly, instead of to the waiting room as a whole.
+        # The waiting room is just a mechanism for us to get the id of the next participant.
+
+        next_participant_id =
+          Magpie.Endpoint.broadcast!(
+            "iterated_lobby:#{AssignmentIdentifier.to_string(socket.assigns.assignment_identifier)}",
+            "finished",
+            %{results: payload["results"]}
+          )
 
         # Send a simple ack reply to the submitting client.
         {:reply, :ok, socket}
@@ -141,4 +152,24 @@ defmodule Magpie.ParticipantChannel do
         {:reply, :error, socket}
     end
   end
+
+  #   defp assign_to_slot_or_to_waiting_queue(socket) do
+  #     case Slots.get_and_set_to_in_progress_next_free_slot(socket.assigns.experiment_id) do
+  #       {:ok, slot_identifier} ->
+  #         broadcast(socket, "slot_available", slot_identifier)
+  #         :ok
+
+  #       :no_free_slot_available ->
+  #         with :ok <- WaitingQueueWorker.queue_participant(socket.assigns.participant_id) do
+  #           {:ok, :queued_up}
+  #         end
+
+  #         broadcast(socket, "waiting_in_queue", %{})
+
+  #         :ok
+
+  #       _ ->
+  #         :error
+  #     end
+  #   end
 end

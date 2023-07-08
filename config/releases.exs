ead04eef0f662655bc9ffaec0dcdbc9cb033eaff
Xiang Ji <hi@xiangji.me>
2023-07-09 00:55:28 +0800

    Try emptying the releases.exs file

--- config/releases.exs
+++ config/releases.exs
@@ -1,6 +1,1 @@
-import Config
-
-config :gigalixir_getting_started, GigalixirGettingStartedWeb.Endpoint,
-  server: true,
-  http: [port: {:system, "PORT"}], # Needed for Phoenix 1.2 and 1.4. Doesn't hurt for 1.3.
-url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
+# This file is just here so that Gigalixir knows to use Elixir releases for deployments."

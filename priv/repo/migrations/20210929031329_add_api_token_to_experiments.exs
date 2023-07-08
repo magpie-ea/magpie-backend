9f04e4dfdfd345de678f66b40ae5340c667af0fa
Xiang Ji <hi@xiangji.me>
2023-07-09 00:55:28 +0800

    Try not to use pgcrypto atm

--- priv/repo/migrations/20210929031329_add_api_token_to_experiments.exs
+++ priv/repo/migrations/20210929031329_add_api_token_to_experiments.exs
@@ -2,10 +2,10 @@ defmodule Magpie.Repo.Migrations.AddApiTokenToExperiments do
   use Ecto.Migration
 
   def change do
-    execute(
-      "CREATE EXTENSION IF NOT EXISTS \"pgcrypto\"",
-      "DROP EXTENSION IF EXISTS \"pgcrypto\""
-    )
+    # execute(
+    #   "CREATE EXTENSION IF NOT EXISTS \"pgcrypto\"",
+    #   "DROP EXTENSION IF EXISTS \"pgcrypto\""
+    # )
 
     alter table(:experiments) do
       add :api_token, :string, null: false, default: fragment("md5(random()::text)")
